36dd900bb1371956560a535911ea3164f52521e9
John Doe <yanisghoul35@gmail.com>
2025-04-04 16:57:28 +0200

    init + test components porsche

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PorscheDesignSystemModule } from '@porsche-design-system/components-angular';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PorscheDesignSystemModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
